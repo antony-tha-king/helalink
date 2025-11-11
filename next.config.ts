213b080122f960c8fb407f4bbdead590aa4def60
Antony Maina <teamantonie@gmail.com>
2025-11-11 05:30:41 +0000

    ok i would like you to know put a redirection that directs to whatsapp w

--- next.config.ts
+++ next.config.ts
@@ -32,6 +32,16 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination:
+          'https://wa.me/254117993236?text=Hi%2C%20tell%20me%20more%20about%20the%20job%20opportunity.',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
